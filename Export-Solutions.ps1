83f26903d03554bb515a53b7998195d6cc3f2bb1
Delsk <delsk@hotmail.com>
2024-01-24 10:37:37 -0500

    Update export and import scripts to not require PowerShell 7 and update comment blocks

--- Export-Solutions.ps1
+++ Export-Solutions.ps1
@@ -1,5 +1,8 @@
-#Requires -Version 7.0
 <#
+ This script expands on the script originally created by Suparna Banerjee and published at
+ https://suparnatechbasket.wordpress.com/2023/07/26/bulk-move-dataverse-solutions/
+ Her page will outline the prerequisites for running this script.
+
  Microsoft provides programming examples for illustration only, without warranty either expressed or
  implied, including, but not limited to, the implied warranties of merchantability and/or fitness
  for a particular purpose.
@@ -13,14 +16,24 @@
  ---------------------------------------------------------------------------------------------------------------------------
  History
  ---------------------------------------------------------------------------------------------------------------------------
+ v1.1 - 01/24/2024 - MARKKOR removed requirement for PowerShell 7 and updated commments to show Suparna's original script
  v1.0 - 01/04/2024 - MARKKOR Created
  ============================================================================================================================
  
- This script is provided as is and without warranty. Please review it closely
- and fully test it in a non-production environment before implementing it
- in a production M365 tenant.
+ This script is provided as is and without warranty. Please review it closely and fully test it in a non-production 
+ environment before implementing it in a production M365 tenant.
 
- Because of the -AsPlainText parameter on ConvertFrom-SecureString, PowerShell 7 is required
+ This is the first of two scripts to run.
+  * This script does the export from the source environment and creates an exports.csv file you will need to edit
+    to show the destination environment of all solutions you want to import.
+  * Import-Solutions.ps1 does the import to the destination environment after you've edited the exports.csv file
+    created by this script.
+  
+  BEFORE RUNNING THIS SCRIPT:
+    * Modify the two variable values below. 
+      (the square brackets are only used to show you where your value goes. They should be removed when you enter your value)
+      * $defaultLocation - the location where you want the exported solutions to be saved
+      * $sourceEnv - the URL of the source environment you want to export from
 #>
 
 # use the same $defaultLocation variable in both the import and export script
@@ -37,14 +50,19 @@ New-Item -ItemType Directory -Force -Path $defaultLocation | Out-Null
 # set the default location
 Set-Location -Path $defaultLocation
 
-# determin the filename for the CSV file to be exported
+# determine the filename for the CSV file to be exported
 $exportCSV = [System.IO.Path]::Combine($defaultLocation, "Exports.csv")
 
 # Get user credentials for source environment
 $userName = Read-Host "Please enter user name"
-$password = ConvertFrom-SecureString(Read-Host -AsSecureString "Please enter password") -AsPlainText
+$password = Read-Host -AsSecureString "Please enter password"
+# because $password will contain a secure string and the authentication command below requires
+# a plain text string, this code converts the secure string to plain text
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+$password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+[Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR)
 
-#Connect to Source Environment
+# Connect to source environment
 Write-Host "Authenticating... "
 pac auth create --name Source --username $username --password $password --url $sourceEnv
 
